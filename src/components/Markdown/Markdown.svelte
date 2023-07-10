943c2fd2f95541521aae43edf3ae99f682bf696c
Neriya Rosner <neri.coder@gmail.com>
2023-07-10 17:27:21 +0300

    create markdown component

---
+++ src/components/Markdown/Markdown.svelte
@@ -0,0 +1,21 @@
+<script lang="ts">
+    export let text: string;
+
+    import {unified} from 'unified'
+    import remarkParse from 'remark-parse'
+    import remarkRehype from 'remark-rehype'
+    import remarkHtml from 'remark-html'
+    import rehypeSanitize from 'rehype-sanitize'
+
+    const content = text ? await unified()
+        .use(remarkParse)
+        .use(remarkHtml)
+        .use(remarkRehype)
+        .use(rehypeSanitize)
+        .process(text) : "";
+</script>
+
+<div class="Markdown">
+    {@html content}
+</div>
+
