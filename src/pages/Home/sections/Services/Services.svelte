855cc95621a3cfd2e16adc20c8fd1fdf20d4f010
Neriya Rosner <neri.coder@gmail.com>
2023-07-10 18:07:39 +0300

    use tabs

--- src/pages/Home/sections/Services/Services.svelte
+++ src/pages/Home/sections/Services/Services.svelte
@@ -2,16 +2,30 @@
 
     import {SERVICES, TEXTS} from "./consts";
     import Service from "./Service.svelte";
+    import Tabs from "../../../../components/Tabs/Tabs.svelte";
+    import TabPanel from "../../../../components/Tabs/TabPanel.svelte";
+    import TabList from "../../../../components/Tabs/TabList.svelte";
+    import Tab from "../../../../components/Tabs/Tab.svelte";
+
 </script>
 
 <div class="Services">
     <h2>{TEXTS.title}</h2>
 
-    <div class="Services__list">
+    <Tabs class="Services__list">
+        <TabList>
+            {#each SERVICES as service}
+                <Tab tab={service.tab}>
+                    {service.tab}
+                </Tab>
+            {/each}
+        </TabList>
         {#each SERVICES as service}
-            <Service service={service}/>
+            <TabPanel>
+                <Service service={service}/>
+            </TabPanel>
         {/each}
-    </div>
+    </Tabs>
 </div>
 
 <style>
@@ -20,6 +34,5 @@
         flex-direction: column;
         align-items: center;
         justify-content: center;
-        height: 100%;
     }
 </style>
