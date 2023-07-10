dcaff8ca506e3bd0d98c09b3a7356994a8c82e3a
Neriya Rosner <neri.coder@gmail.com>
2023-07-10 17:26:39 +0300

    initial commit

---
+++ src/App.svelte
@@ -0,0 +1,17 @@
+<script lang="ts">
+    import {Home} from './pages/Home'
+</script>
+
+<main>
+    <Home />
+</main>
+
+<style>
+    main {
+        display: flex;
+        flex-direction: column;
+        align-items: center;
+        justify-content: center;
+        height: 100dvh;
+    }
+</style>
