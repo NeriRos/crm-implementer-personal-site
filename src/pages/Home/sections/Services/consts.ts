0b8a8bac30c289931eb8cc6626ef663a8bb25f2d
Neriya Rosner <neri.coder@gmail.com>
2023-07-10 18:07:23 +0300

    lines

--- src/pages/Home/sections/Services/consts.ts
+++ src/pages/Home/sections/Services/consts.ts
@@ -5,54 +5,56 @@ export const TEXTS = {
 export const SERVICES = [
     {
         tab: "Setup & Integration",
-        content: `## You've Chosen the Right CRM
-            
-            Now it's time to set you up for success and ensure you have the right processes in place to empower you with valuable insights.
-            
-            ### What's Included?
-            
-            - CRM Setup & Integration
-            - Database Migration
-            - Quality Assurance of Data
-            - Email Tracking & Scheduling
-            - Integration of Sales Team's Emails with HubSpot (Gmail)`
+        content: `
+## You've Chosen the Right CRM
+Now it's time to set you up for success and ensure you have the right processes in place to empower you with valuable insights.
+
+### What's Included?
+
+- CRM Setup & Integration
+- Database Migration
+- Quality Assurance of Data
+- Email Tracking & Scheduling
+- Integration of Sales Team's Emails with HubSpot (Gmail)`
     },
     {
         tab: "Inbound Marketing",
-        content: `## Educate and Nurture
+        content: `
+## Educate and Nurture
             
-            Receive technical and strategic guidance on setting up and using Marketing Hub to boost traffic, convert leads, and achieve your marketing goals.
-            
-            Build a solid infrastructure to deliver relevant content and engaging experiences for leads and customers.
-            
-            Additionally, we'll optimize your marketing processes using automation strategies and AI tools.
-            
-            Simplify and scale your marketing efforts to generate qualified leads for your sales team.
-            
-            ### What's Included?
-            
-            - Lead Generation & Lead Flow
-            - Email and Marketing Automation Strategies
-            - Campaign Reporting
-            - Content Creation - Crafting persuasive content in multiple variations, supported by compelling visuals.`
+Receive technical and strategic guidance on setting up and using Marketing Hub to boost traffic, convert leads, and achieve your marketing goals.
+
+Build a solid infrastructure to deliver relevant content and engaging experiences for leads and customers.
+
+Additionally, we'll optimize your marketing processes using automation strategies and AI tools.
+
+Simplify and scale your marketing efforts to generate qualified leads for your sales team.
+
+### What's Included?
+
+- Lead Generation & Lead Flow
+- Email and Marketing Automation Strategies
+- Campaign Reporting
+- Content Creation - Crafting persuasive content in multiple variations, supported by compelling visuals.`
     },
     {
         tab: "Sales Enablement",
-        content: `## Show Me the Money
-            
-            Receive technical and strategic guidance on setting up and using Sales Hub to drive traffic, convert leads, and achieve your sales goals.
-            
-            Manage your processes in HubSpot, allowing you to focus on what you do best - selling your product.
-            
-            We'll audit your current processes, provide improvement strategies, and automate daily tasks using industry best practices.
-            
-            Simplify and scale your sales process to turn qualified leads into successful deals.
-            
-            ### What's Included?
-            
-            - Account-Based Marketing (ABM) Tools and Automation
-            - Establishing a Robust Sales Funnel
-            - Sequences
-            - Content Creation - Craft impactful sales emails that can be game-changers for your business.`
+        content: `
+## Show Me the Money
+
+Receive technical and strategic guidance on setting up and using Sales Hub to drive traffic, convert leads, and achieve your sales goals.
+
+Manage your processes in HubSpot, allowing you to focus on what you do best - selling your product.
+
+We'll audit your current processes, provide improvement strategies, and automate daily tasks using industry best practices.
+
+Simplify and scale your sales process to turn qualified leads into successful deals.
+
+### What's Included?
+
+- Account-Based Marketing (ABM) Tools and Automation
+- Establishing a Robust Sales Funnel
+- Sequences
+- Content Creation - Craft impactful sales emails that can be game-changers for your business.`
     }
 ]
