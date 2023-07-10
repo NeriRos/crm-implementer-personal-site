ccfa0dd50a804b178a4224e7253948ed9468bafe
Neriya Rosner <neri.coder@gmail.com>
2023-07-10 17:27:28 +0300

    add home

---
+++ src/pages/Home/Home.svelte
@@ -0,0 +1,12 @@
+<script lang="ts">
+    import Hero from "./sections/Hero/Hero.svelte";
+    import Services from "./sections/Services/Services.svelte";
+</script>
+
+<Hero />
+<Services />
+<!--<Projects />-->
+<!--<Approach />-->
+<!--<Contact />-->
+<!--<About />-->
+
