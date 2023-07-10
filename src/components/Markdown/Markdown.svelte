6fb3a8f476bf385780a38712fbb5a1d607ca322d
Neriya Rosner <neri.coder@gmail.com>
2023-07-10 18:07:04 +0300

    await promise

--- src/components/Markdown/Markdown.svelte
+++ src/components/Markdown/Markdown.svelte
@@ -1,21 +1,19 @@
 <script lang="ts">
-    export let text: string;
-
     import {unified} from 'unified'
     import remarkParse from 'remark-parse'
-    import remarkRehype from 'remark-rehype'
     import remarkHtml from 'remark-html'
-    import rehypeSanitize from 'rehype-sanitize'
 
-    const content = text ? await unified()
+    export let text: string;
+
+    const contentPromise = text ? unified()
         .use(remarkParse)
         .use(remarkHtml)
-        .use(remarkRehype)
-        .use(rehypeSanitize)
         .process(text) : "";
 </script>
 
 <div class="Markdown">
-    {@html content}
+    {#await contentPromise then content}
+        {@html content}
+    {/await}
 </div>
 
