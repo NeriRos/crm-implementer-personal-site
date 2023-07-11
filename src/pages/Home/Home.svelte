86cd825722966594dcacd4b1175df37c97772ec2
Neriya Rosner <neri.coder@gmail.com>
2023-07-11 13:41:36 +0300

    divide to sections

--- src/pages/Home/Home.svelte
+++ src/pages/Home/Home.svelte
@@ -3,12 +3,35 @@
     import {Services} from "./sections/Services";
     import {Approach} from "./sections/Approach";
     import {Contact} from "./sections/Contact";
+    import {Packages} from "./sections/Packages";
 </script>
 
-<Hero/>
-<Services/>
-<!--<Projects />-->
-<Approach/>
-<Contact />
+<div class="Home">
+    <div class="Home__header">
+        <Hero/>
+    </div>
+    <div class="Home__content">
+        <Services/>
+        <Packages/>
+        <!--<Projects />-->
+        <Approach/>
+    </div>
+    <div class="Home__footer">
+        <Contact/>
+    </div>
+</div>
 <!--<About />-->
 
+<style>
+    .Home__content :global( > *) {
+        margin-bottom: 2rem;
+    }
+
+    .Home__content {
+        padding: 1rem;
+    }
+
+    .Home__footer {
+        padding: 2rem 0 4rem 0;
+    }
+</style>
